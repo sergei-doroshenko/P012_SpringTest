f236b2573d4b384dc96d800335442776021adebc
sergei-doroshenko <6823298@gmail.com>
2017-01-28 18:38:18 +0300

    Fixed after rebase issues

--- spring-hiber/src/main/java/org/sergei/hiber/domain/Order.java
+++ spring-hiber/src/main/java/org/sergei/hiber/domain/Order.java
@@ -2,14 +2,7 @@ package org.sergei.hiber.domain;
 
 
 import java.util.List;
-import javax.persistence.CascadeType;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.OneToMany;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 /**
  * Created by Sergei_Doroshenko on 12/5/2016.
