814a2bdb845efe66d87db6d276e52a00dc967231
sdoroshenko <VieFP1l-K3Yw>
2017-01-28 16:15:14 +0300

    Added Tag entity

--- spring-hiber/src/main/java/org/sergei/hiber/domain/Order.java
+++ spring-hiber/src/main/java/org/sergei/hiber/domain/Order.java
@@ -1,9 +1,15 @@
 package org.sergei.hiber.domain;
 
 
-
-
-import javax.persistence.*;
+import java.util.List;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
 
 /**
  * Created by Sergei_Doroshenko on 12/5/2016.
@@ -14,11 +20,14 @@ public class Order {
 
     @Id
     @GeneratedValue(strategy=GenerationType.AUTO)
-    @Column(name = "order_id")
+//    @Column(name = "order_id")
     private Long id;
 
     @Column(name = "order_description")
     private String description;
+
+    @OneToMany(mappedBy = "order", cascade = {CascadeType.ALL}, orphanRemoval = true)
+    private List<Tag> tags;
 
     public Long getId () {
         return id;
@@ -36,11 +45,21 @@ public class Order {
         this.description = description;
     }
 
+    public List<Tag> getTags() {
+        return tags;
+    }
+
     @Override
-    public String toString () {
+    public String toString() {
         return "Order{" +
                 "id=" + id +
                 ", description='" + description + '\'' +
+                /*", tags=" + (tags == null ? "[]" : tags)  +*/
                 '}';
     }
+    /*
+    org.hibernate.LazyInitializationException: failed to lazily initialize
+    a collection of role: org.sergei.hiber.domain.Order.tags,
+    could not initialize proxy - no Session
+     */
 }
--- spring-hiber/src/main/resources/test-data.sql
+++ spring-hiber/src/main/resources/test-data.sql
@@ -1,1 +1,3 @@
-INSERT INTO orders (order_id, order_description) VALUES (1, 'test');
+INSERT INTO orders (id, order_description) VALUES (1, 'test');
+
+INSERT INTO tag (id, value, order_id) VALUES (1, 'tag1', 1), (2, 'tag2', 1);
